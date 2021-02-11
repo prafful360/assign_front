7b702c358516cc864f5db6ac8b8ff0d81224f0f5
prafful360 <praffulranjan54@gmail.com>
2021-02-12 04:20:44 +0530

    Posts

--- src/components/user/userComponents/posts.js
+++ src/components/user/userComponents/posts.js
@@ -1,36 +1,51 @@
 import axios from "axios";
 import React, { Component } from "react";
 import { Link, withRouter, Redirect } from "react-router-dom";
-import Home from "../../core/home";
 
 class UserPosts extends Component {
   state = {
     posts: [],
+    loading: false,
   };
 
   componentDidMount() {
+    this.setState({ loading: true });
     const userId = this.props.match.params.userId;
     axios
       .get(`https://jsonplaceholder.typicode.com/users/${userId}/posts`)
       .then((res) => {
         const posts = res.data;
-        this.setState({ posts });
+        this.setState({ posts: posts, loading: false });
         // console.log(res.data);
       });
     // console.log(this.props); // For confirmation of fetching userid from props
   }
-  renderUserPosts = (posts) => (
-    <>
-      {posts.map((post, i) => (
-        <div className="card-text text-capitalize" key={i}>
-          Title :
-          <Link to={`/post/${post.id}`}>{post.title.substring(0, 15)}...</Link>
-          <hr />
-        </div>
-      ))}
-    </>
+  renderUserPosts = (posts) => {
+    if (posts.length === "0") {
+      return <div>No Posts</div>;
+    } else {
+      return (
+        <>
+          {posts.map((post, i) => (
+            <div className="card-text text-capitalize" key={i}>
+              Title :
+              <Link to={`/post/${post.id}`}>
+                {post.title.substring(0, 15)}...
+              </Link>
+              <hr />
+            </div>
+          ))}
+        </>
+      );
+    }
+  };
+  loaderDiv = () => (
+    <div className="container text-align-c">
+      <div class="spinner-border text-primary " role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
   );
-
   render() {
     return (
       <div className="container">
@@ -38,7 +53,9 @@ class UserPosts extends Component {
         <div className="card ">
           <div className="card-header">Posts by the user</div>
           <div className="card-body">
-            {this.renderUserPosts(this.state.posts)}
+            {this.state.loading
+              ? this.loaderDiv()
+              : this.renderUserPosts(this.state.posts)}
           </div>
         </div>
       </div>
